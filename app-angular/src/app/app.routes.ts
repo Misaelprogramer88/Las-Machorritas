5540ad9264c92e3f4a56095c235d9352db55f2e3
pepevazquez-4 <23040139@alumno.utc.edu.mx>
2025-10-09 18:50:17 -0600

    feat: Adicion de la estructura y archivos del proyecto Angular

---
+++ app-angular/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { PerfilCardComponent } from './perfil-card/perfil-card.component';
+
+export const routes: Routes = [
+    { path: '', component: PerfilCardComponent } 
+];
